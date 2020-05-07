f49ba01b3f8a6b0f127df0acf36c42366f7662a9
Chernyshov_Nikita <xpom5566@mail.ru>
2020-05-07 22:46:50 +0300

    feat: learning process added on each input for 2_multi-input_multi-output file =)

--- 2_multi-input_multi-output.py
+++ 2_multi-input_multi-output.py
@@ -33,41 +33,6 @@ def vect_mat_mul(vect: List[float], matrix: List[List[float]]) -> List[float]:
   return output
 
 
-def neural_network(input: List[float], weights: List[List[float]]) -> float:
-  pred = vect_mat_mul(input, weights)
-
-  return pred
-
-
-# Input Data [toes[i], wlrec[i], nfans[i]]
-toes = [8.5, 9.5, 9.9, 9]  # toes - текущее ср.число игр за сезон между игроками
-wlrec = [0.65, 0.8, 0.8, 0.9]  # win / lose
-nfans = [1.2, 1.3, 0.5, 1.0]  # number of fans (millions)
-
-# Output Data [hurt[i], win[i], sad[i]]
-hurt = [0.1, 0, 0, 0.1]  # hutred players
-win = [1, 1, 0, 1]  # They will win ?
-sad = [0.1, 0, 0.1, 0.2]  # Sadness =(
-
-alpha = 0.01
-
-input = [toes[0], wlrec[0], nfans[0]]
-true = [hurt[0], win[0], sad[0]]
-print('input', input)
-print('true', true)
-
-pred = neural_network(input, weights)
-print('pred', pred)
-
-error = [0, 0, 0]
-delta = [0, 0, 0]
-
-for i in range(len(true)):
-  error[i] = (pred[i] - true[i])**2
-  delta[i] = pred[i] - true[i]
-print('errors', error)
-
-
 # matrix with [X * Y] size matrix filled with zeroes
 def zeros_matrix(sizeX, sizeY):
   matrix = [0] * sizeX
@@ -86,5 +51,88 @@ def outer_pred(vec_a, vec_b):
   return out
 
 
-weight_deltas = outer_pred(input, delta)
-print('weight_deltas', weight_deltas)
+def neural_network(input: List[float], weights: List[List[float]]) -> float:
+  pred = vect_mat_mul(input, weights)
+
+  return pred
+
+
+# Input Data [toes[i], wlrec[i], nfans[i]]
+toes = [8.5, 9.5, 9.9, 9]  # toes - текущее ср.число игр за сезон между игроками
+wlrec = [0.65, 0.8, 0.8, 0.9]  # win / lose
+nfans = [1.2, 1.3, 0.5, 1.0]  # number of fans (millions)
+
+# Output Data [hurt[i], win[i], sad[i]]
+hurt = [0.1, 0, 0, 0.1]  # hutred players
+win = [1, 1, 0, 1]  # They will win ?
+sad = [0.1, 0, 0.1, 0.2]  # Sadness =(
+
+alpha = 0.009
+
+# input = [toes[0], wlrec[0], nfans[0]]
+# trueArray = [hurt[0], win[0], sad[0]]
+# print('input', input)
+# print('true', true)
+
+
+# learning function
+def learnIteration(inputIterationNumber: int, isShowPrints=False):
+  input = [toes[inputIteration], wlrec[inputIteration], nfans[inputIteration]]
+  trueArray = [hurt[inputIteration], win[inputIteration], sad[inputIteration]]
+
+  pred = neural_network(input, weights)
+  # print('pred', pred)
+
+  error = [0, 0, 0]
+  delta = [0, 0, 0]
+
+  for i in range(len(trueArray)):
+    # Cube error, firstly solve big problems,
+    # and only after it, solve small problems
+    error[i] = (pred[i] - trueArray[i])**2
+    #
+    delta[i] = pred[i] - trueArray[i]
+    if isShowPrints:
+      print('errors', error)
+    weight_deltas = outer_pred(input, delta)
+    # print('weight_deltas', weight_deltas)
+
+    for j in range(len(weights[i])):
+      weights[i][j] -= weight_deltas[j][i] * alpha
+
+
+for ii in range(300):
+  # Change weights after each input
+  for inputIteration in range(len(toes)):
+    learnIteration(inputIteration)
+
+learnIteration(0, True)
+'''
+alpha = 0.001
+ii = 25000
+
+errors [0.0011077411992017857, 0, 0]
+errors [0.0011077411992017857, 0.01770334244762078, 0]
+errors [0.0011077411992017857, 0.01770334244762078, 0.00035452208105551984]
+errors [0.002495604692682505, 0, 0]
+errors [0.002495604692682505, 0.000422134330389761, 0]
+errors [0.002495604692682505, 0.000422134330389761, 0.0099318711176652]
+errors [0.0047715867202401206, 0, 0]
+errors [0.0047715867202401206, 0.011624229312993135, 0]
+errors [0.0047715867202401206, 0.011624229312993135, 0.006578673673701122]
+errors [4.7293735817031664e-05, 0, 0]
+errors [4.7293735817031664e-05, 0.001245905980574472, 0]
+errors [4.7293735817031664e-05, 0.001245905980574472, 8.956777941957558e-05]
+errors [0.001107726101314892, 0, 0]
+errors [0.001107726101314892, 0.01770266701784014, 0]
+errors [0.001107726101314892, 0.01770266701784014, 0.0003544733836479419]
+
+-----------------
+alpha = 0.009
+ii = 300
+
+learnIteration(0, True)
+errors [0.0008073293403828909, 0, 0]
+errors [0.0008073293403828909, 0.01051218484162111, 0]
+errors [0.0008073293403828909, 0.01051218484162111, 0.00020163150186320943]
+'''
